79d764389d063b4f9802d0a798bae7d9c134f706
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 11:33:14 -0300

    adicionando watchers

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import extensions.format
+import watchers.DecimalTextWatcher
 
 class MainActivity : AppCompatActivity() {
 
@@ -73,6 +74,8 @@ class MainActivity : AppCompatActivity() {
         btCalcular.setOnClickListener {
             calcular()
         }
+        etPeso.addTextChangedListener(DecimalTextWatcher(etPeso, 1))
+        etAltura.addTextChangedListener(DecimalTextWatcher(etAltura, 2))
     }
 
 }
